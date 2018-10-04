99daaf37489fe93d2567fa7132989403ef0bcfdf
Brian Francis <13bfrancis@gmail.com>
2018-10-04 18:42:46 -0400

    fixed path issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  pathPrefix: '/gatsby',
+  pathPrefix: '/first-site',
   plugins: [
     'gatsby-plugin-react-helmet',
     {
